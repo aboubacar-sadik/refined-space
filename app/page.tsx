3dbdf59287fa8eadb0a286c1ad13e1d5ec9f1e81
aboubacar-sadik <80213601+aboubacar-sadik@users.noreply.github.com>
2026-03-02 15:33:58 +0100

    Vercel Deployments

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-import { GET_ARTICLE_BY_SLUG_QUERY } from "@/lib/queries";
-import { sanityFetch } from "@/sanity/lib/live";
 import { Footer } from "@/sections/Footer";
 import { Navigation } from "@/sections/Navigation";
 
 export default async function Home() {
-  const { data: posts } = await sanityFetch({
-    query: GET_ARTICLE_BY_SLUG_QUERY,
-    params: { slug: "avocado-green-mattress-is-it-worth-usd2-000" },
-  });
-
   return (
     <>
       <Navigation />
