b8f14975a536fc104b56c996e3e291380871300f
aboubacar-sadik <80213601+aboubacar-sadik@users.noreply.github.com>
2026-03-14 00:39:37 +0100

    Refactor article page by removing unused queries and components

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -2,53 +2,24 @@ import { sanityFetch } from "@/sanity/lib/live";
 import {
   GET_ALL_CATEGORIES_QUERY,
   GET_ARTICLE_BY_SLUG_QUERY,
-  GET_POPULAR_ARTICLES_QUERY,
-  GET_RECENT_ARTICLES_QUERY,
-  GET_RELATED_ARTICLES_QUERY,
 } from "@/lib/queries";
 import { Footer } from "@/sections/Footer";
 import { Navigation } from "@/sections/Navigation";
-import { ArticleHero } from "@/sections/ArticleHero";
 import { notFound } from "next/navigation";
-import { ArticleContent } from "@/sections/ArticleContent";
-import { PopularArticles } from "@/components/PopularArticles";
-import { CategoriesList } from "@/components/article/CategoriesList";
-import { NewsletterSignup } from "@/components/article/NewsletterSignup";
-import { RelatedArticles } from "@/sections/article/RelatedArticles";
 
 export default async function Page({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  const { slug } = await params;
 
   const { data: categories } = await sanityFetch({
     query: GET_ALL_CATEGORIES_QUERY,
   });
 
-  const { data: articles } = await sanityFetch({
-    query: GET_RECENT_ARTICLES_QUERY,
-  });
-
-  const { data: popular_articles } = await sanityFetch({
-    query: GET_POPULAR_ARTICLES_QUERY,
-  });
-
   const { data: article } = await sanityFetch({
     query: GET_ARTICLE_BY_SLUG_QUERY,
     params: params,
-  });
-
-  const categorySlugs = article?.categories?.map((c) => c.slug);
-  const tagSlugs = article?.tags?.map((c) => c.slug);
-  const { data: related_articles } = await sanityFetch({
-    query: GET_RELATED_ARTICLES_QUERY,
-    params: {
-      slug: slug,
-      categorySlugs,
-      tagSlugs,
-    },
   });
 
   if (!article) {
@@ -60,43 +31,8 @@ export default async function Page({
       {/* <ReadingProgress /> */}
       <Navigation />
       <main className="pt-20">
-        {/* Article Hero */}
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <ArticleHero article={article} />
-        </div>
-
-        {/* Main Content Area */}
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-16">
-          <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 lg:gap-12">
-            {/* Main Article Content */}
-            <div className="lg:col-span-8 pb-14">
-              <ArticleContent body={article.body}/>
-            </div>
-
-            {/* Sidebar */}
-            <aside className="lg:col-span-4 space-y-6 ">
-              {/* Popular Articles */}
-              <PopularArticles articles={popular_articles} />
-
-              {/* Categories */}
-              <CategoriesList categories={categories} />
-
-              {/* Newsletter */}
-              <NewsletterSignup />
-            </aside>
-          </div>
-
-          {/* Author Bio */}
-          {/* <AuthorBio author={author} /> */}
-
-          {/* Article Navigation */}
-          {/* <ArticleNavigation previous={prevArticle} next={nextArticle} /> */}
-
-          {/* Related Articles */}
-          <RelatedArticles articles={related_articles} />
-        </div>
+         Main Content Area 
       </main>
-
       <Footer categories={categories} />
     </div>
   );
