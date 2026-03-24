ba762e7ea6fa27600cd92fdbc94fa0aa0b7162a6
aboubacar-sadik <80213601+aboubacar-sadik@users.noreply.github.com>
2026-03-24 19:08:42 +0100

    REFACTORING

--- components/page-builder.tsx
+++ components/page-builder.tsx
@@ -2,8 +2,26 @@ import { PAGE_QUERYResult } from "@/sanity.types";
 import Hero from "./blocks/hero";
 import OurMission from "./blocks/our-mission";
 
+// type PageBuilderProps = {
+//   content: NonNullable<PAGE_QUERYResult>["content"];
+// };
+
+type HeroBlock = {
+  _type: "heroSimple";
+  _key: string;
+  // other props used in <Hero />
+};
+
+type OurMissionBlock = {
+  _type: "ourMission";
+  _key: string;
+  // other props used in <OurMission />
+};
+
+type PageBlock = HeroBlock | OurMissionBlock;
+
 type PageBuilderProps = {
-  content: NonNullable<PAGE_QUERYResult>["content"];
+  content: PageBlock[];
 };
 
 export function PageBuilder({ content }: PageBuilderProps) {
@@ -13,14 +31,15 @@ export function PageBuilder({ content }: PageBuilderProps) {
 
   return (
     <main>
-      {content.map((block) => {
+      {content.map((block: { _key: string; _type: string }) => {
+        if (!("_type" in block)) return null;
+
         switch (block._type) {
           case "heroSimple":
             return <Hero key={block._key} {...block} />;
           case "ourMission":
             return <OurMission key={block._key} {...block} />;
           default:
-            // This is a fallback for when we don't have a block type
             return <div key={block._key}>Block not found: {block._type}</div>;
         }
       })}
